5500e509d8fffb265153408785f8fd574c9b0e79
Piglet- <cferreira6594@gmail.com>
2015-04-23 00:25:43 -0430

    Casos Prueba

--- IngSoftware_Tarea2/test_calcularPrecio.py
+++ IngSoftware_Tarea2/test_calcularPrecio.py
@@ -7,6 +7,7 @@ Created on 21/4/2015
 import unittest
 from calcularPrecio import Tarifa,calcularPrecio
 from datetime import datetime
+from decimal import Decimal
 
 class Test_calcularPrecio(unittest.TestCase):
 
@@ -16,10 +17,7 @@ class Test_calcularPrecio(unittest.TestCase):
         ini_reserva = datetime(2015, 4, 21, 6, 15)
         fin_reserva = datetime(2015, 4, 21, 6, 15)
         tiempo_reserva = [ini_reserva,fin_reserva]
-        try:
-            calcularPrecio(tarifa_de_prueba, tiempo_reserva) 
-        except:
-            pass          
+        self.assertRaises(Exception,calcularPrecio,tarifa_de_prueba, tiempo_reserva)
         
     def test0Tarifa(self):
         # Caso de prueba Tarifa(0,0)
@@ -36,11 +34,7 @@ class Test_calcularPrecio(unittest.TestCase):
         ini_reserva = datetime(2015, 4, 21, 6, 15)
         fin_reserva = datetime(2015, 4, 21, 6, 29)
         tiempo_reserva = [ini_reserva,fin_reserva]
-        
-        try:
-            calcularPrecio(tarifa_de_prueba, tiempo_reserva) 
-        except:
-            pass          
+        self.assertRaises(Exception,calcularPrecio,tarifa_de_prueba, tiempo_reserva)       
         
     def test15MTimeSmallT(self):
         # Caso de prueba tiempo de reservacion = (15) minutos, tarifa pequenia
@@ -58,7 +52,7 @@ class Test_calcularPrecio(unittest.TestCase):
         fin_reserva = datetime(2015, 4, 28, 6, 15)
         tiempo_reserva = [ini_reserva,fin_reserva]
         precio = calcularPrecio(tarifa_de_prueba, tiempo_reserva)
-        self.assertEqual(precio,((5.50*5)+(7.25*2)))
+        self.assertEqual(precio,((5.50*5*24)+(7.25*2*24)))
     
     def test7Days1MinTimer(self):
         # Caso de prueba tiempo de reservacion = (7) dias, (1) minuto
@@ -66,11 +60,7 @@ class Test_calcularPrecio(unittest.TestCase):
         ini_reserva = datetime(2015, 4, 21, 6, 15)
         fin_reserva = datetime(2015, 4, 28, 6, 16)
         tiempo_reserva = [ini_reserva,fin_reserva]
-        
-        try:
-            calcularPrecio(tarifa_de_prueba, tiempo_reserva)
-        except:
-            pass
+        self.assertRaises(Exception,calcularPrecio,tarifa_de_prueba, tiempo_reserva)
        
     
     def testNegativeTimer(self):
@@ -79,11 +69,7 @@ class Test_calcularPrecio(unittest.TestCase):
         ini_reserva = datetime(2015, 4, 21, 6, 15)
         fin_reserva = datetime(2015, 4, 20, 6, 15)
         tiempo_reserva = [ini_reserva,fin_reserva]
-        
-        try:
-            calcularPrecio(tarifa_de_prueba, tiempo_reserva)
-        except:
-            pass
+        self.assertRaises(Exception,calcularPrecio,tarifa_de_prueba, tiempo_reserva)
         
     def testNegativeTarifa(self):
         # Caso de prueba tarifa negativa
@@ -91,11 +77,7 @@ class Test_calcularPrecio(unittest.TestCase):
         ini_reserva = datetime(2015, 4, 21, 6, 15)
         fin_reserva = datetime(2015, 4, 27, 6, 15)
         tiempo_reserva = [ini_reserva,fin_reserva]
-        
-        try:
-            calcularPrecio(tarifa_de_prueba, tiempo_reserva)
-        except:
-            pass
+        self.assertRaises(Exception,calcularPrecio,tarifa_de_prueba, tiempo_reserva)
         
     def testImaginaryTarifa(self):
         # Caso de prueba tarifa imaginaria
@@ -103,7 +85,7 @@ class Test_calcularPrecio(unittest.TestCase):
         ini_reserva = datetime(2015, 4, 21, 6, 15)
         fin_reserva = datetime(2015, 4, 27, 6, 15)
         tiempo_reserva = [ini_reserva,fin_reserva]
-        calcularPrecio(tarifa_de_prueba, tiempo_reserva)
+        self.assertRaises(TypeError,calcularPrecio,tarifa_de_prueba, tiempo_reserva)
         
     def testCharTimer(self):
         # Caso de prueba tiempo de reservacion con caracteres
@@ -111,15 +93,15 @@ class Test_calcularPrecio(unittest.TestCase):
         ini_reserva = datetime(2015, 4, 21, 6, 15)
         fin_reserva = datetime(2015, 4, 27, 6, 15)
         tiempo_reserva = [ini_reserva,fin_reserva]
-        calcularPrecio(tarifa_de_prueba, tiempo_reserva)
+        self.assertRaises(TypeError,calcularPrecio,tarifa_de_prueba, tiempo_reserva)
         
-    def testDecimalTimer(self):
-        # Caso de prueba tiempo de reservacion decimal
+    def testDiferentTimer(self):
+        # Caso de prueba tiempo de reservacion de tipo entero
         tarifa_de_prueba = Tarifa(5,7)
-        ini_reserva = datetime(2015, 4, 21, 6, 15.3)
-        fin_reserva = datetime(2015, 4, 27, 6, 16.5)
+        ini_reserva = 100
+        fin_reserva = 101
         tiempo_reserva = [ini_reserva,fin_reserva]
-        calcularPrecio(tarifa_de_prueba, tiempo_reserva)
+        self.assertRaises(TypeError,calcularPrecio,tarifa_de_prueba, tiempo_reserva)
     
     def testDecimal1Tarifa(self):
         # Caso de prueba Tarifa decimal 1
@@ -127,16 +109,16 @@ class Test_calcularPrecio(unittest.TestCase):
         ini_reserva = datetime(2015, 4, 21, 6, 15)
         fin_reserva = datetime(2015, 4, 27, 6, 15)
         tiempo_reserva = [ini_reserva,fin_reserva]
-        calcularPrecio(tarifa_de_prueba, tiempo_reserva)
+        self.assertRaises(Exception,calcularPrecio,tarifa_de_prueba, tiempo_reserva)
         
     def testDecimal2Tarifa(self):
         # Caso de prueba Tarifa decimal 2
         tarifa_de_prueba = Tarifa(5.4,7.6)
         ini_reserva = datetime(2015, 4, 21, 6, 15)
-        fin_reserva = datetime(2015, 4, 27, 6, 15)
+        fin_reserva = datetime(2015, 4, 28, 6, 15)
         tiempo_reserva = [ini_reserva,fin_reserva]
         precio = calcularPrecio(tarifa_de_prueba, tiempo_reserva)
-        self.assertEqual(precio,((5.4*5)+ 7.6))
+        self.assertEqual(precio,(Decimal((5.4*5*24)+ 7.6*2*24).quantize(Decimal('1.00'))))
         
     def testSecondsTimer(self):
         # Caso de prueba tiempo de reservacion con segundos
@@ -145,14 +127,16 @@ class Test_calcularPrecio(unittest.TestCase):
         fin_reserva = datetime(2015, 4, 22, 6, 15, 3)
         tiempo_reserva = [ini_reserva,fin_reserva]
         calcularPrecio(tarifa_de_prueba, tiempo_reserva)
+        self.assertRaises(Exception,calcularPrecio,tarifa_de_prueba, tiempo_reserva)
 
     def testBigTarifa(self):
         # Caso de prueba tarifa muy grande
         tarifa_de_prueba = Tarifa(2**31,2**64)
         ini_reserva = datetime(2015, 4, 21, 6, 15)
-        fin_reserva = datetime(2015, 4, 27, 6, 15)
+        fin_reserva = datetime(2015, 4, 28, 6, 15)
         tiempo_reserva = [ini_reserva,fin_reserva]
-        calcularPrecio(tarifa_de_prueba, tiempo_reserva)
+        precio = calcularPrecio(tarifa_de_prueba, tiempo_reserva)
+        self.assertEqual(precio, (((2**31)*5*24) + (2**64)*2*24))
         
     def testTimerMax(self):
         # Caso de prueba tiempo de reservacion maximo
@@ -160,11 +144,7 @@ class Test_calcularPrecio(unittest.TestCase):
         ini_reserva = datetime.max
         fin_reserva = datetime(2015, 4, 27, 6, 15)
         tiempo_reserva = [ini_reserva,fin_reserva]
-        
-        try:
-            calcularPrecio(tarifa_de_prueba, tiempo_reserva)
-        except:
-            pass
+        self.assertRaises(Exception,calcularPrecio,tarifa_de_prueba, tiempo_reserva)
             
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
